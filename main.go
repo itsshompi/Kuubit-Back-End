9ba0dae29b6d77a5a2842f3d32d5783f90387ee0
Felipe Schneeberger <itsshompi@gmail.com>
2017-03-02 01:22:28 -0300

    Login Handler

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 const (
@@ -10,13 +16,63 @@ const (
 	pubKeyPath  = "keys/app.rsa.pub"
 )
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
+type userCredentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+	var user userCredentials
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Fprintf(w, "Error in request")
+		return
+	}
+
+	fmt.Println(user.Username, user.Password)
+	if strings.ToLower(user.Username) == "shompi" && user.Password == "1234567" {
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		claims["name"] = "Jon Snow"
+		claims["admin"] = true
+		claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+		t, err := token.SignedString([]byte("secret"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintln(w, "Error while signing the token")
+			log.Printf("Error signing token: %v\n", err)
+			return
+		}
+		json, err := json.Marshal(t)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(json)
+	} else {
+		w.WriteHeader(http.StatusForbidden)
+		fmt.Println("Error logging in")
+		fmt.Fprint(w, "Invalid credentials")
+		return
+	}
+
+}
+
+func helloServer(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("{name: Kuubit API, version: 1.0.1}"))
 }
 
 func main() {
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", helloServer)
+	http.HandleFunc("/login", loginHandler)
 	err := http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
